ab14a8973f1e5e8b715a94660bb9aa4d240a37bd
MushysWebs <141169926+MushysWebs@users.noreply.github.com>
2024-03-20 13:37:49 -0600

    Finished lab 3, added a useState hook to map my list and pass tasks from props

--- App.js
+++ App.js
@@ -1,13 +1,20 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { SafeAreaView, StyleSheet } from 'react-native';
 import ToDoList from './components/ToDoList.jsx';
 import ToDoForm from './components/ToDoForm.jsx';
 
 
 function App() {
+
+  const [tasks, setTasks] = useState([
+    'Do laundry',
+    'Go to gym',
+    'Walk dog',
+  ])
+
   return (
     <SafeAreaView style={{flex: 1, paddingTop: 40, paddingBottom: 20,}}>
-      <ToDoList />
+      <ToDoList tasks={tasks}/>
       <ToDoForm />
     </SafeAreaView>
   );
