0750c686572ca042984db2aab33902241a42c7bd
MushysWebs <141169926+MushysWebs@users.noreply.github.com>
2024-03-20 14:33:09 -0600

    Finished lab 4, added some styilng with a title aswell as a remove button.

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { SafeAreaView, StyleSheet } from 'react-native';
+import { SafeAreaView, Text, View, StyleSheet } from 'react-native';
 import ToDoList from './components/ToDoList.jsx';
 import ToDoForm from './components/ToDoForm.jsx';
 
@@ -12,12 +12,46 @@ function App() {
     'Walk dog',
   ])
 
+  const addTask = (taskText) => {
+    if (taskText && !tasks.includes(taskText)) {
+      setTasks([...tasks, taskText])
+    }
+  }
+
+  const removeTask = (index) => {
+    setTasks(tasks.filter((task, taskIndex) => index !== taskIndex));
+  };
+
   return (
-    <SafeAreaView style={{ flex: 1, paddingTop: 40, paddingBottom: 20, }}>
-      <ToDoList tasks={tasks} />
-      <ToDoForm />
+    <SafeAreaView style={styles.container}>
+      <View style={styles.header}>
+        <Text style={styles.title}>Get Er Done</Text>
+      </View>
+      <ToDoList tasks={tasks} removeTask={removeTask} />
+      <ToDoForm addTask={addTask} />
     </SafeAreaView>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingTop: 40,
+    paddingBottom: 20,
+  },
+  header: {
+    padding: 5,
+    backgroundColor: '#f0f0f0',
+    borderRadius: 20,
+    borderWidth: 1,
+    borderColor: '#ccc',
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    textAlign: 'center',
+
+  },
+});
 
 export default App;
--- components/ToDoList.jsx
+++ components/ToDoList.jsx
@@ -1,15 +1,16 @@
 import React from 'react';
-import { ScrollView, Pressable, View, Text, StyleSheet } from 'react-native';
+import { ScrollView, View, Text, StyleSheet, TouchableOpacity } from 'react-native';
 
-function ToDoList({ tasks }) {
+function ToDoList({ tasks, removeTask }) {
   return (
     <ScrollView>
       {tasks.map((task, index) => (
-        <Pressable key={index}>
-          <View style={styles.task}>
-            <Text style={styles.taskText}>{task}</Text>
-          </View>
-        </Pressable>
+        <View key={index} style={styles.task}>
+          <Text style={styles.taskText}>{task}</Text>
+          <TouchableOpacity onPress={() => removeTask(index)} style={styles.removeButton}>
+            <Text style={styles.removeButtonText}>X</Text>
+          </TouchableOpacity>
+        </View>
       ))}
     </ScrollView>
   );
@@ -17,15 +18,24 @@ function ToDoList({ tasks }) {
 
 const styles = StyleSheet.create({
   task: {
-    
+    flexDirection: 'row',
+    justifyContent: 'space-between',
     padding: 10,
     borderBottomWidth: 1,
     borderColor: '#ccc',
-  },
-  completed: {
-    backgroundColor: '#e0e0e0',
+    alignItems: 'center',
   },
   taskText: {
+    fontSize: 16,
+  },
+  removeButton: {
+    marginLeft: 10,
+    padding: 5,
+    backgroundColor: '#b30039',
+    borderRadius: 5,
+  },
+  removeButtonText: {
+    color: 'white',
     fontSize: 16,
   },
 });
